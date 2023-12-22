96931f331f3d0bdb68df47673d6b158993ee9962
kristinalucic <klucic97@gmail.com>
2023-12-22 13:52:54 +0100

    kockice

---
+++ Kockice - Starting Files/index.js
@@ -0,0 +1,22 @@
+var randomNumber1 = Math.random();
+randomNumber1 = randomNumber*6;
+randomNumber1 = Math.floor(randomNumber1)+1;
+
+var randomNumber2 = Math.random();
+randomNumber2 = randomNumber*6;
+randomNumber2 = Math.floor(randomNumber2)+1;
+
+document.querySelector(".img1").setAttribute("src","images/dice"+ randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+ randomNumber2 + ".png");
+
+if(randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "✨Player 1 win✨";
+}
+
+if(randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "🎇Player 2 win🎇";
+}
+
+else{
+    document.querySelector("h1").innerHTML = "draw";
+}
